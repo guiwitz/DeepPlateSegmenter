6208a39ab0de92ac975e55a112cb6bda0c2007a9
Guillaume Witz <guiwitz@gmail.com>
2018-03-24 21:47:18 +0100

    First commit

---
+++ MMdata.py
@@ -0,0 +1,257 @@
+import os
+import re
+from PIL import Image
+import struct
+import numpy as np
+import pandas as pd
+from PIL import Image
+
+class MMData:
+    """Parsing of MicroManager metadata"""
+    def __init__(self, folder = None, tiffs = None, mm_meta = None, height = None, 
+                 width = None, mm_map = None, interval = None, channels = None,  num_planes = None):
+        """Standard __init__ method.
+
+        Parameters
+        ----------
+        folder : str
+            Folder containing the acquisition.
+        tiffs : : list of str
+            List of tiffs composing the acquisition.
+        mm_meta : str
+            String of MicroManager metadata
+        height : int
+            Image height
+        width : int
+            Image widht
+        mm_map : pandas dataframe
+            Dataframe indicating the multi-dimensional position of each frame in the acquisition
+        interval : float
+            Time between frames of time-lapse
+        channels : list of str
+            List of channel names
+        num_planes : list of int
+            Number of planes per channel
+        """
+        
+        self.folder = folder
+        self.tiffs = self.get_all_tiffs()
+        self.mm_meta = mm_meta
+        self.height = height
+        self.height = self.get_image_height()
+        self.width = width
+        self.width = self.get_image_width()
+        self.mm_map = mm_map
+        self.interval = interval
+        self.channels = channels
+        self.channels = self.get_channels()
+        self.num_planes = num_planes
+        self.num_planes = self.get_number_planes()
+    
+    def get_all_tiffs(self):
+        """Return list of all files composing an acquisition"""
+        image_files = [re.search('.*(MMStack).*', f).group(0) for f in os.listdir(self.folder) if re.search('.*(MMStack).*ome.tif', f)]
+        return image_files
+        
+    def get_first_tiff(self):
+        """Return name of first .tif block of the acquisition"""
+        first_chunk = [f for f in self.tiffs if not re.search('.*(MMStack.ome).*',f)==None][0]
+        return first_chunk
+        
+    def get_last_tiff(self):
+        """Return name of last .tif block of the acquisition"""
+        last_chunk = self.tiffs[np.argmax([int(re.search('(?<=MMStack_)(\d+)',f).group(0)) if re.search('.*(MMStack.ome).*',f)==None else 0 for f in self.tiffs])]
+        return last_chunk
+    
+    def get_MM_metadata(self):
+        """Return MicroManager metadata string contained in Tag 50839"""
+        with open(self.folder+'/'+self.get_first_tiff(), "rb") as binary_file:
+
+            # Seek position and read N bytes
+            binary_file.seek(0)  # Go to beginning
+            byte_order = struct.unpack('<ss',binary_file.read(2))
+            tiff_id = struct.unpack('<H',binary_file.read(2))
+            ifd_pos = struct.unpack('<L',binary_file.read(4))
+
+            binary_file.seek(ifd_pos[0])
+            nb_tags = struct.unpack('<H',binary_file.read(2))[0]
+
+            for i in range(nb_tags+1):
+                binary_file.seek(ifd_pos[0])
+                binary_file.seek(ifd_pos[0]+2+(i-1)*12)
+
+                tagID = struct.unpack('<H',binary_file.read(2))[0]
+                if tagID ==50839:
+                    struct.unpack('<H',binary_file.read(2))
+                    offset_size = struct.unpack('<L',binary_file.read(4))[0]
+                    offset = struct.unpack('<L',binary_file.read(4))[0]
+                    break;
+            binary_file.seek(offset)
+            metadata = struct.unpack('<'+str(offset_size)+'s',binary_file.read(offset_size))
+            metadata_str = str(metadata)
+            metadata_str = metadata_str.replace('\\x00','')
+            metadata_str = metadata_str.replace("\\", "")
+            self.mm_meta = metadata_str
+            return metadata_str
+    
+    def get_image_height(self):
+        """Return image height in px"""
+        if self.height is None:
+            if self.mm_meta is None:
+                self.get_MM_metadata()
+                
+            height = int(re.findall('(?:(?<=Height":)|(?<=Height":")|(?<=Height":\{"PropVal":"))(\d+).*', self.mm_meta)[0])
+            #height = int(re.findall('(?:((?<=Height":")|(?<=Height":\{"PropVal":")))(\d+).*', self.mm_meta)[0])
+            self.height = height
+        return self.height
+    
+    def get_image_width(self):
+        """Return image width in px"""
+        if self.width is None:
+            if self.mm_meta is None:
+                self.get_MM_metadata()
+            width = int(re.findall('(?:(?<=Width":)|(?<=Width":")|(?<=Width":\{"PropVal":"))(\d+).*', self.mm_meta)[0])
+            self.width = width
+        return self.width
+    
+    def get_interval(self):
+        """Return time-lapse interval"""
+        if self.interval is None:
+            if self.mm_meta is None:
+                self.get_MM_metadata()
+            interval = int(re.findall('(?<=WaitInterval":)|(?<=WaitInterval":")(\d+).*', self.mm_meta)[0])
+            self.interval = interval
+        return self.interval
+    
+    
+    def get_map_indices(self):
+        """Return the index map of the acquisition as a list of dict
+        
+        The returned dataframe contains 7 keys giving for each row its exact multi-dimensional position.
+        Rows have no defined order. The keys are:
+        channel: which channel in a multic-wavelength acquisition
+        slice: which slice in a z-stack acqusition
+        frame: which time-point in a time-lapse acquisition
+        position: which position in a multi-position acquisition
+        image_index: which image in a given tiff chunk
+        offset: binaray offset in a given tiff chunk
+        chunk: which tiff chunk in case the acquisition is >4GB and split in severla chunks
+        """
+        item_order = ('channel','slice','frame','position','offset')
+        if self.mm_map is None:
+            mm_map = {'channel':[], 'slice':[], 'frame':[], 'position':[], 'image_index':[], 'offset':[], 'chunk':[]}
+            for f in self.tiffs:
+                with open(self.folder+'/'+f, "rb") as binary_file:
+
+                    # Seek position and read N bytes
+                    binary_file.seek(0)  # Go to beginning
+                    byte_order = struct.unpack('<ss',binary_file.read(2))
+                    tiff_id = struct.unpack('<H',binary_file.read(2))
+                    ifd_pos = struct.unpack('<L',binary_file.read(4))
+                    map_tag = struct.unpack('<L',binary_file.read(4))
+                    map_pos = struct.unpack('<L',binary_file.read(4))
+
+                    binary_file.seek(map_pos[0])
+                    binary_file.read(4)
+
+                    nb_images = struct.unpack('<L',binary_file.read(4))
+
+                    for i in range(nb_images[0]):
+                        for x in item_order:
+                            mm_map[x].append(struct.unpack('<L',binary_file.read(4))[0])
+
+                        mm_map['chunk'].append(f)
+                        mm_map['image_index'].append(i)
+            self.mm_map = pd.DataFrame(mm_map)
+        return self.mm_map
+    
+    #@profile
+    def get_image(self, frame=0,channel=0,plane=0,position=0, compress = 1):
+        """Return image at a given frame, channel, plane, position. One can skip every n'th pixel by setting 
+        compress to n"""
+        if self.mm_map is None:
+            self.get_map_indices()
+        selected = self.mm_map.loc[(self.mm_map['frame'] == frame) & (self.mm_map['position']==position) &
+                                 (self.mm_map['channel']==channel) & (self.mm_map['slice']==plane),
+                                 ['chunk','image_index','offset']].values
+        
+        #img = Image.open(self.folder+'/'+selected[0,0])
+        #img.seek(selected[0,1])
+        #image = img.getdata()
+        #image = np.reshape(np.array(image),newshape=[img.height,img.width])
+        
+        with open(self.folder+'/'+selected[0,0], "rb") as binary_file:
+
+            ifd_pos = selected[0,2]
+            binary_file.seek(ifd_pos)  # Go to beginning
+            nb_tags = struct.unpack('<H',binary_file.read(2))[0]
+            #binary_file.seek(mm_map[0:1].offset.values[0])
+    
+            binary_file.seek(ifd_pos+2+(nb_tags)*12)
+            next_ifd = struct.unpack('<L',binary_file.read(4))[0]
+            im_bytes = struct.unpack('<'+str(self.height*self.width)+'H',binary_file.read(2*self.height*self.width))
+                        
+            width = self.width
+            height = self.height
+            
+            #im_bytes = np.array(im_bytes)
+            im_bytes = np.fromiter(im_bytes, np.float)
+            image = np.reshape(im_bytes,newshape=[self.height,self.width])
+            #image = np.array([im_bytes[i:i + width:compress] for i in range(0, len(im_bytes), compress*width)])
+
+        return image
+    
+    def get_stack(self, frame=0,channel=0,position=0, compress = 1):
+        """Return complete z-stack for given frame, channel, position"""
+        mm_map = self.get_map_indices()
+        planes = mm_map[(mm_map['frame']==0)&(mm_map['position']==0)&(mm_map['channel']==channel)]['slice'].values
+        stack = np.empty((self.height,self.width,planes.shape[0]))
+        for i in range(planes.shape[0]):
+            stack[:,:,i] = self.get_image(frame=frame,channel=channel,plane=planes[i],position=position, compress = compress)
+        return stack
+        
+    
+    def get_max_frame(self):
+        """Return total number of time-points in the acquisition"""
+        self.maxframe = self.get_map_indices().frame.values.max()
+        return self.maxframe
+    
+    def get_channels(self):
+        """Return channels of the acquisition"""
+        self.channels = re.findall('(?<=ChNames":\[)|(?<=ChNames":"\[)(.*?)(?=\].*)',self.get_MM_metadata())[0].replace('"','').split(',')
+        return self.channels
+    
+    def get_zstep(self):
+        """Return size of the z-step in stack acquisition in nm"""
+        z_step = float(re.findall('(?:(?<=z-step_um":)|(?<=z-step_um":"))([\d\.]+).*', self.get_MM_metadata())[0])*1000
+        return z_step
+    
+    def get_number_planes(self):
+        """Return number of planes for each channel"""
+        if self.num_planes is None:
+
+            indexmap = self.get_map_indices()
+            num_planes = [indexmap.loc[(indexmap['frame'] == 0) & (indexmap['position']==0) & (indexmap['channel']==i),
+                                       ['chunk','image_index','offset']].values.shape[0] 
+                          for i in range(len(self.get_channels()))]
+            self.num_planes = num_planes
+        return self.num_planes
+    
+    
+def onselect(eclick, erelease):
+    'eclick and erelease are matplotlib events at press and release'
+    print(' startposition : (%f, %f)' % (eclick.xdata, eclick.ydata))
+    print(' endposition   : (%f, %f)' % (erelease.xdata, erelease.ydata))
+    print(' used button   : ', eclick.button)
+
+def toggle_selector(event):
+    print(' Key pressed.')
+    if event.key in ['Q', 'q'] and toggle_selector.RS.active:
+        print(' RectangleSelector deactivated.')
+        toggle_selector.RS.set_active(False)
+    if event.key in ['A', 'a'] and not toggle_selector.RS.active:
+        print(' RectangleSelector activated.')
+        toggle_selector.RS.set_active(True)
+
+
+    
---
+++ platesegmenter.py
@@ -0,0 +1,319 @@
+import numpy as np
+import scipy.ndimage as nd
+import scipy.signal
+import re
+from skimage.feature import match_template
+
+from keras.models import Model
+from keras.layers import Input, concatenate, Conv2D, MaxPooling2D, Conv2DTranspose, Reshape, Flatten
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint
+from keras import backend as K
+
+K.set_image_data_format('channels_last')  # TF dimension ordering in this code
+smooth = 1.
+
+def phase_corr_simple(stack, thickness, z_step):
+    """Return phase correlation image based on a bright field stack
+    
+    Parameters
+    ----------
+    stack : 3D numpy array
+        stack to be correlated
+    thickness: float 
+        Thickness of cell [nm] (typically 800)
+    z_step: float
+        Size of steps between planes [nm]
+        
+    Returns
+    -------
+    correlated_norm : 2D numpy array
+        Correlation image
+    """
+    corr_stack = stack
+    nbplanes = stack.shape[2]
+    middle_plane = round(nbplanes/2)-1
+    
+    heightpos = z_step*(np.arange(stack.shape[2])-middle_plane)
+    
+    gaussderiv = -heightpos*np.exp(-heightpos**2/(2*thickness**2))
+    gaussderiv = gaussderiv/np.linalg.norm(gaussderiv)
+    #height_vals = height_vals[np.newaxis,np.newaxis,...]
+    
+    height_vals = np.empty(stack.shape)
+    height_vals[:] = gaussderiv
+    
+    image_norm = corr_stack/np.resize(np.sqrt(np.sum(corr_stack*corr_stack,axis =2)),
+                                      (nbplanes, corr_stack.shape[0],corr_stack.shape[1])).transpose(1,2,0)
+    
+    #correlated_norm = ndimage.convolve(image_norm, height_vals, mode='constant', cval=0.0)
+    #correlated_norm = scipy.signal.convolve(image_norm, height_vals, mode='valid')
+    correlated_norm = image_norm*height_vals
+    correlated_norm=np.sum(correlated_norm,axis =2)
+    
+    return correlated_norm
+
+def phase_corr(stack, z0, thickness, z_step):
+    """Return phase correlation image based on a bright field stack
+    
+    Parameters
+    ----------
+    stack : 3D numpy array
+        stack to be correlated
+    z0 : int
+        index of "central" plane (cell signal extinction)
+    thickness: float 
+        Thickness of cell [nm] (typically 800)
+    z_step: float
+        Size of steps between planes [nm]
+        
+    Returns
+    -------
+    correlated_norm : 2D numpy array
+        Correlation image
+    """
+    corr_stack = np.zeros(stack.shape)
+    nbplanes = stack.shape[2]
+    full_mask = np.zeros(stack.shape)
+    middle_plane = round(nbplanes/2)
+    #z0 = middle_plane*np.ones(stack.shape[0:2])
+    #z0[0,2]=10
+    delta_per_pix = np.min(np.stack((nbplanes-z0,z0-1),axis=2),axis = 2)
+
+    for i in range(nbplanes):
+
+        #masking of pixels outside allowed range
+        mask = 1-(np.abs(i-z0)>delta_per_pix)
+        full_mask[:,:,i] = mask
+
+        #distance to center matrix
+        height_mask=i-z0
+        height_mask =z_step*height_mask
+
+        #Calculate derivative of Gaussian for whole image
+        gaussderiv = -height_mask*np.exp(-height_mask**2/(2*thickness**2))
+        gaussderiv = gaussderiv*mask
+        corr_stack[:,:,i]=gaussderiv
+    
+    image_masked = stack*full_mask
+    image_norm = image_masked/np.resize(np.sqrt(np.sum(image_masked*image_masked,axis =2)),(nbplanes, image_masked.shape[0],image_masked.shape[1])).transpose(1,2,0)
+    gauss_norm = corr_stack/np.resize(np.sqrt(np.sum(corr_stack*corr_stack,axis =2)),(nbplanes, corr_stack.shape[0],corr_stack.shape[1])).transpose(1,2,0)
+    correlated_norm = gauss_norm* image_norm
+    correlated_norm=np.sum(correlated_norm,axis =2)
+    
+    return correlated_norm
+
+def steerable1(image, alpha=0, sigma=1):
+    """Return firsr order gaussian steerable filtering of an image
+    
+    Parameters
+    ----------
+    image : numpy array
+        Image to be filtered
+    alpha : float
+        Angle of steerable filter
+    sigma : width of filter
+    
+    Returns
+    -------
+    filtered : 2D numpy array
+        Filtered image
+    """
+    support = np.floor(4*sigma)
+    if support<1:
+        support = 1
+    x = np.arange(-support, support+1)
+    xx, yy = np.meshgrid(x,x)
+    
+    G1_0 = -(xx/sigma**2)*np.exp(-(xx**2+yy**2)/(2*sigma**2))/(sigma*np.sqrt(2*np.pi));
+    G1_90 = -(yy/sigma**2)*np.exp(-(xx**2+yy**2)/(2*sigma**2))/(sigma*np.sqrt(2*np.pi));
+
+
+    Ix = nd.convolve(image,G1_0)
+    Iy = nd.convolve(image,G1_90)
+    
+    filtered = np.cos(theta)*Ix+np.sin(theta)*Iy
+    return filtered
+
+
+def steerable2(image, alpha=0, sigma=1):
+    """Return second order gaussian steerable filtering of an image
+    
+    Parameters
+    ----------
+    image : numpy array
+        Image to be filtered
+    alpha : float
+        Angle of steerable filter
+    sigma : width of filter
+    
+    Returns
+    -------
+    filtered : 2D numpy array
+        Filtered image
+    """
+    support = np.floor(4*sigma)
+    if support<1:
+        support = 1
+    x = np.arange(-support, support+1)
+    xx, yy = np.meshgrid(x,x)
+    
+    g0 = np.exp(-(xx**2+yy**2)/(2*sigma**2))/(sigma*np.sqrt(2*np.pi))
+    G2a = -g0/sigma**2+g0*xx**2/sigma**4
+    G2b = g0*xx*yy/sigma**4
+    G2c = -g0/sigma**2+g0*yy**2/sigma**4
+    
+    I2a = nd.convolve(image,G2a)
+    I2b = nd.convolve(image,G2b)
+    I2c = nd.convolve(image,G2c)
+    
+    filtered = (np.cos(alpha))**2*I2a+np.sin(alpha)**2*I2c-2*np.cos(alpha)*np.sin(alpha)*I2b
+    
+    return filtered
+
+def steerable2_crosscorr(image, alpha=0, sigma=1):
+    """Return second order normalized cross-correletion gaussian steerable filtering of an image
+    
+    Parameters
+    ----------
+    image : numpy array
+        Image to be filtered
+    alpha : float
+        Angle of steerable filter
+    sigma : width of filter
+    
+    Returns
+    -------
+    filtered : 2D numpy array
+        Filtered image
+    """
+    support = np.floor(4*sigma)
+    if support<1:
+        support = 1
+    x = np.arange(-support, support+1)
+    xx, yy = np.meshgrid(x,x)
+    
+    g0 = np.exp(-(xx**2+yy**2)/(2*sigma**2))/(sigma*np.sqrt(2*np.pi))
+    G2a = -g0/sigma**2+g0*xx**2/sigma**4
+    G2b = g0*xx*yy/sigma**4
+    G2c = -g0/sigma**2+g0*yy**2/sigma**4
+    
+    filt = (np.cos(alpha))**2*G2a+np.sin(alpha)**2*G2c-2*np.cos(alpha)*np.sin(alpha)*G2b
+    filtered = match_template(image,filt,pad_input=True)
+    
+    return filtered
+
+def get_position_names(MMobj):
+    """Return position (A1-Site0, B3-Site10 etc) names and well names (A1, B3 etc) of the acquisition"""
+    positions = re.findall('(?:(?<=Label":"))([a-zA-Z0-9_-]+)', MMobj.get_MM_metadata())
+    wells = np.unique([re.findall('([a-zA-Z0-9_]+)(?:(?=-Site))', x)[0] for x in positions])
+
+    return positions, wells
+
+
+output =True
+
+def dice_coef(y_true, y_pred):
+    smooth = 1
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+
+def get_unet(dims,img_rows,img_cols):
+    inputs = Input((img_rows, img_cols, dims))
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool3)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(pool4)
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv5)
+
+    up6 = concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(conv5), conv4], axis=3)
+    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(up6)
+    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv6)
+
+    up7 = concatenate([Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(conv6), conv3], axis=3)
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(up7)
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv7)
+
+    up8 = concatenate([Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(conv7), conv2], axis=3)
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(up8)
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv8)
+
+    up9 = concatenate([Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(conv8), conv1], axis=3)
+    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(up9)
+    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv9)
+
+    conv10 = Conv2D(1, (1, 1), activation='sigmoid')(conv9)
+    
+    conv11 = Reshape((img_rows*img_cols,1),input_shape=(img_rows,img_cols,1))(conv10)
+
+    model = Model(inputs=[inputs], outputs=[conv11])
+    
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=[dice_coef], sample_weight_mode='temporal')
+        
+    return model
+
+
+def plate_deeptrain(folder, img_rows,img_cols, dims):
+
+    imgs_train, imgs_mask_train, imgs_weight_train  = load_train_data(folder)
+
+    imgs_train = imgs_train.astype('float32')
+
+    imgs_mask_train = imgs_mask_train[..., np.newaxis]
+    imgs_mask_train = imgs_mask_train.astype('float32')
+    imgs_mask_train /= 255.  # scale masks to [0, 1]
+    
+    imgs_weight_train = imgs_weight_train.astype('float32')
+
+    plate_model = get_unet(dims,img_rows,img_cols)
+    model_checkpoint = ModelCheckpoint(folder+'weights.h5', monitor='val_loss', save_best_only=True)
+
+    plate_model.fit(imgs_train, imgs_mask_train, batch_size=32, nb_epoch=100, verbose=1, shuffle=True,
+              validation_split=0.2,sample_weight = imgs_weight_train,
+              callbacks=[model_checkpoint])
+
+
+    imgs_test, imgs_id_test = load_test_data(folder)
+
+    imgs_test = imgs_test.astype('float32')
+    
+    plate_model.load_weights(folder+'weights.h5')
+
+    imgs_mask_test = plate_model.predict(imgs_test, verbose=1)
+    #np.save('imgs_mask_test.npy', imgs_mask_test)
+
+    test_dir = folder+'test'
+    if not os.path.exists(test_dir):
+        os.mkdir(pred_dir)
+    for image, image_id in zip(imgs_mask_test, imgs_id_test):
+        image = np.reshape(image,(img_rows,img_cols))
+        image = (image * 255.).astype(np.uint8)
+        imsave(os.path.join(test_dir, str(image_id) + '_pred.png'), image)
+        
+def load_train_data(folder):
+    imgs_train = np.load(folder+'imgs_train.npy')
+    imgs_mask_train = np.load(folder+'imgs_mask_train.npy')
+    imgs_weight_train = np.load(folder+'imgs_weight_train.npy')
+    return imgs_train, imgs_mask_train, imgs_weight_train
+
+def load_test_data(folder):
+    imgs_test = np.load(folder+'imgs_test.npy')
+    imgs_id = np.load(folder+'imgs_id_test.npy')
+    return imgs_test, imgs_id
+
