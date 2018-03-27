d0d0f5b6d7e56bb45b1b1ec50cd9239d09bab761
Guillaume Witz <guiwitz@gmail.com>
2018-03-27 18:45:52 +0200

    created package

---
+++ deeplate/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='deeplate',
+      version='0.1',
+      description='Bacteria microscopy image segmentation using deep learning ',
+      url='https://github.com/guiwitz/DeepPlateSegmenter',
+      author='Guillaume Witz',
+      author_email='',
+      license='MIT',
+      packages=['deeplate'],
+      zip_safe=False,
+      #install_requires=['numpy','skimage','scipy','keras'],
+      )
