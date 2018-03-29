d3bfae702242495f17170c964b700d756807d779
Guillaume Witz <guiwitz@gmail.com>
2018-03-29 14:32:35 +0200

    minor

--- cluster/deep_plate_train_cluster.sh
+++ cluster/deep_plate_train_cluster.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#SBATCH --job-name=test_JOB-gpu \n')
-#SBATCH --time=06:00:00 \n')
-#SBATCH --mem-per-cpu=8G \n')
-#SBATCH --ntasks=1 \n')
-#SBATCH --cpus-per-task=1 \n')
-#SBATCH --qos=6hours \n')
-#SBATCH --partition=k80 \n')
-#SBATCH --gres=gpu:1 \n')
+#SBATCH --job-name=test_JOB-gpu
+#SBATCH --time=06:00:00
+#SBATCH --mem-per-cpu=8G
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --qos=6hours
+#SBATCH --partition=k80
+#SBATCH --gres=gpu:1
 #SBATCH -e /scicore/home/nimwegen/witzg/DeepLearningData/Learn20170126/training_error.log
 #SBATCH -o /scicore/home/nimwegen/witzg/DeepLearningData/Learn20170126/training_output.log
 
